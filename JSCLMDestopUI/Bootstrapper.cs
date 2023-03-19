c597b53f8c1205b71e3780c3c82bad865a585771
Mike Beer <mike@thebeerfamily.com>
2023-03-19 14:51:06 +0000

    Added The Destop UI
    Added Caliburn.Micro and bootstrp to MainView

---
+++ JSCLMDestopUI/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using JSCLMDestopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JSCLMDestopUI;
+
+public class Bootstrapper : BootstrapperBase
+{
+    public Bootstrapper()
+    {
+        Initialize();
+    }
+
+    protected override void OnStartup(object sender, StartupEventArgs e)
+    {
+        DisplayRootViewForAsync<MainViewModel>();
+    }
+}
